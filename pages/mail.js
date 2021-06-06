09cf03322f9511c76caf513dadb97134c5cd5bda
vinaypugal <vinay.chennai17@gmail.com>
2021-06-06 16:29:27 +0530

    fixed bugs

--- pages/mail.js
+++ pages/mail.js
@@ -1,3 +1,5 @@
+import Footer from "@/components/Footer";
+import Header from "@/components/Header";
 import { useUser } from "@auth0/nextjs-auth0";
 import { useRouter } from "next/router";
 import React, { useEffect } from "react";
@@ -12,14 +14,18 @@ const mail = () => {
     }
   }, []);
   return (
-    <div className="d-flex justify-content-center align-items-center" style={{height: "100vh" , width: "100vw"}}>
-    <div className="d-flex justify-content-center align-items-center p-3" style={{height: "100px" , width: "auto" , backgroundColor : "#4FB2BF" , borderRadius : "15px"}}>
-      <span>
+    <>
+    <Header />
+    <div className="d-flex justify-content-center align-items-center" style={{height: "80vh" , width: "100vw"}}>
+    <div className="d-flex justify-content-center align-items-center p-3" style={{height: "100px" , width: "auto" , backgroundColor : "#4FB2BF" , borderRadius : "15px" , color : "white"}}>
+      <b>
         Please verify your e-mail to gain full access , Make sure that you
         re-login after verfiying your e-mail
-      </span>
+      </b>
     </div>
     </div>
+    <Footer/>
+    </>
   );
 };
 
