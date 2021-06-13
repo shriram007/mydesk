b206273025875149ad9d8d0717e1f79c1e13d8e6
vinaypugal <vinay.chennai17@gmail.com>
2021-06-14 00:20:25 +0530

    .

--- pages/api/tutor/index.js
+++ pages/api/tutor/index.js
@@ -1,10 +1,10 @@
+import Tutor from "@/models/tutor";
+import Tutorprofile from "@/models/tutorprofile";
+import Subject from "@/models/subject";
 import connectDB from "@/middleware/connectDB";
 import { getSession, withApiAuthRequired } from "@auth0/nextjs-auth0";
 import mongoose from "mongoose";
 import nc from "next-connect";
-import Tutor from "@/models/tutor";
-import Tutorprofile from "@/models/tutorprofile";
-import Subject from "@/models/subject";
 const handler = nc();
 
 handler.use(connectDB);
